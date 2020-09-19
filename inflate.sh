af8bcda138b83d9949154b1ffc83ee5e867efa3e
Hetsh <git@hetsh.de>
2020-09-19 12:55:53 +0200

    Assert inflated size by chunk

--- inflate.sh
+++ inflate.sh
@@ -30,29 +30,38 @@ COMPRESSED_FILE_SIZE=$(stat --printf="%s" $Z_FILE)
 HEADER_SIZE=$((COMPRESSED_FILE_SIZE-COMPRESSED_DATA_SIZE))
 debug "Debug: Header size is $HEADER_SIZE bytes."
 
-# Chunks and their length
+# Compressed data chunk length and resulting file size
 declare -a CHUNKS_LEN
+declare -a FILES_SIZE
 for (( CHUNKS=0; CHUNKS*16<=HEADER_SIZE-48; CHUNKS++ )); do
 	OFFSET=$((32+CHUNKS*16))
 	LEN=$(extract_uint $OFFSET)
 	CHUNKS_LEN+=($LEN)
+
+	OFFSET=$((OFFSET+8))
+	SIZE=$(extract_uint $OFFSET)
+	FILES_SIZE+=($SIZE)
 done
 debug "Debug: Contains $CHUNKS chunks."
 
-# Extract chunks and concatenate
+# Decompression
 rm -f "$FILE"
 OFFSET=$((1+$HEADER_SIZE))
-for LEN in "${CHUNKS_LEN[@]}"; do
-	{ printf "\x1f\x8b\x08\x00\x00\x00\x00\x00"; tail -c +$OFFSET "$Z_FILE" | head -c $LEN; } | zcat >> "$FILE" || true
+TOTAL=0
+for (( I=0; I<CHUNKS; I++ )); do
+	# Inflate and concatenate
+	LEN="${CHUNKS_LEN[I]}"
+	{ printf "\x1f\x8b\x08\x00\x00\x00\x00\x00"; tail -c +$OFFSET "$Z_FILE" | head -c $LEN; } | zcat 2> /dev/null >> "$FILE" && echo "Error: Unexpected gzip success!" || debug "Debug: Expected gzip error occured."
 	OFFSET=$((OFFSET+LEN))
-done
 
-# Assert file size
-EXPECTED_FILE_SIZE=$(extract_uint 24)
-ACTUAL_FILE_SIZE=$(stat --printf="%s" $FILE)
-if [ $ACTUAL_FILE_SIZE -ne $EXPECTED_FILE_SIZE ]; then
-	echo "Error: File size mismatch!"
-	exit 1
-fi
+	# Assert inflated size
+	SIZE=${FILES_SIZE[I]}
+	TOTAL=$((TOTAL+SIZE))
+	EXPECTED=$(stat --printf="%s" $FILE)
+	if [ $EXPECTED -ne $TOTAL ]; then
+		echo "Error: Chunk $I inflated size does not match!"
+		exit 2
+	fi
+done
 
 echo "Success: Inflated $Z_FILE."
