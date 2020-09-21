4de78e036fcf86bd3b97de26d740d09621d540d1
Hetsh <git@hetsh.de>
2020-09-21 20:46:26 +0200

    Added function to append hex value to binary file

--- extract.sh
+++ extract.sh
@@ -6,3 +6,11 @@ extract_uint() {
 	local FILE="$2"
 	head -c $((OFFSET+WIDTH)) "$FILE" | tail -c $WIDTH | od --format u --width=$WIDTH --address-radix=n | tr -d ' '
 }
+
+append_hex() {
+	local WIDTH=4
+	local HEX=$(printf "%0$((WIDTH*2))X" "$1")
+	local HEX_BE=$(echo ${HEX:6:2}${HEX:4:2}${HEX:2:2}${HEX:0:2} | sed -e 's/../\\x&/g')
+	local FILE="$2"
+	echo -n -e "$HEX_BE" >> "$FILE"
+}
